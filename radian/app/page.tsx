da3d06ab00ba36f464143f681d1da7c6096660d3
rohitlodhii <rohitlodhi0225@gmail.com>
2026-01-08 22:55:42 +0530

    add fixes

--- radian/app/page.tsx
+++ radian/app/page.tsx
@@ -6,11 +6,10 @@ import Language from "@/components/core/language";
 import Navbar from "@/components/core/Navbar";
 import VideoPlayer from "@/components/core/player";
 import System from "@/components/core/system";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-[1400px] mx-auto w-full h-full border-l border-r border-primary/40 ">
+    <div className="max-w-350 mx-auto w-full h-full border-l border-r border-primary/40 ">
       <Navbar/>
       <Hero/>
       <VideoPlayer/>
